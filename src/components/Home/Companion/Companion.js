8d928f0f2b58c8687a6a59bbba2b465b02e15e56
Manan Doshi <manandoshi1301@gmail.com>
2023-03-05 02:49:40 +0530

    Added companion cards

--- src/components/Home/Companion/Companion.js
+++ src/components/Home/Companion/Companion.js
@@ -1,39 +1,20 @@
-import React from 'react'
-import { Col, Card, Button, Ratio, Row } from 'react-bootstrap';
-import { Chip } from '@material-ui/core';
-function Companion() {
-    const data = ["HIKING", "Swimmimg", "Running"]
-  
-    return (
-        <div>
-            <Col xs={12} sm={12} md={6} lg={4} className='p-0 my-1 p-3'>
-                <Card className='border-0' style={{ backgroundColor: '#F7F7F7', width: '18rem', height: '5%' }}>
-                    <Card.Img variant="top" src="https://newprofilepic2.photo-cdn.net//assets/images/article/profile.jpg" style={{ backgroundColor: '#F7F7F7', width: '18rem', height: '5%' }} />
-                    <Card.Body>
-                    <div className='d-flex align-items-end justify-content-between'></div>
-                        <Card.Subtitle className="mb-2 text-muted ">Alan,22</Card.Subtitle>
-                    <div className='d-flex align-items-end justify-content-between'></div>
-                        <Card.Subtitle className=" mb-2 text-muted ">Mumbai</Card.Subtitle>
-                        <Card.Text>
-                           
-                            {data.map((interest,index) => (
-              <Chip
-                key={index}
-                size='small'
-                label={interest}
-                margin= '5px 5px 5px 0px'
-  
-              />))}
-                            
-                        </Card.Text>
-                        <div className='d-flex align-items-end justify-content-between'>
+import React from 'react';
+import CompanionCard from "./CompanionCard";
+import { Col, Row, Container } from 'react-bootstrap';
 
-                            <Button variant="primary">Connect</Button>
-                        </div>
-                    </Card.Body>
-                </Card>
-            </Col>
-        </div>
+const Companion = ({ companionData }) => {
+    return (
+        <Container fluid className='p-0'>
+            <Row>
+                {
+                    companionData.map((item, key) => (
+                        <Col key={key} xs={12} sm={12} md={6} lg={3}>
+                            <CompanionCard user={item.user} />
+                        </Col>
+                    ))
+                }
+            </Row>
+        </Container>
     )
 }
 
