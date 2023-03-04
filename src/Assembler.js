def2591bedfa981f86ee3d631eebd2ba0797d09e
Manan Doshi <manandoshi1301@gmail.com>
2023-03-04 12:51:41 +0530

    Added bootstrap link, fontawesome and navbar

--- src/Assembler.js
+++ src/Assembler.js
@@ -1,11 +1,14 @@
 import React, { useContext } from 'react'
+import BrandNavbar from './components/common/BrandNavbar'
 // import { AppBinderContext } from './AppBinderContext';
 
 const Assembler = () => {
   // const [name] = useContext(AppBinderContext);
 
   return (
-    <div>Assembler</div>
+    <>
+      <BrandNavbar />
+    </>
   )
 }
 
