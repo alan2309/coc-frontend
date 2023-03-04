4279f9daf85cf1c70c2b0a19f6a5e78d58e4a37b
chaitanya_1911 <chaitanya.kumbhar@think360.ai>
2023-03-04 17:31:43 +0530

    added axios instance

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,38 +1,41 @@
-import React,{useState} from 'react'
-import BrandNavbar from '../common/BrandNavbar'
-import { Container, Form } from 'react-bootstrap'
+import React, { useState, useEffect } from "react";
+import BrandNavbar from "../common/BrandNavbar";
+import { Container, Form } from "react-bootstrap";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 import usePlacesAutocomplete, {
   getGeocode,
   getLatLng,
 } from "use-places-autocomplete";
 import {
-    Combobox,
-    ComboboxInput,
-    ComboboxPopover,
-    ComboboxList,
-    ComboboxOption,
-  } from "@reach/combobox";
-  import "@reach/combobox/styles.css";
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
+import "@reach/combobox/styles.css";
+import axiosInstance from "../../axiosInstance";
 
 const LandingPage = () => {
-    const { isLoaded } = useLoadScript({
-        googleMapsApiKey: "AIzaSyC_2ABFZTAwaDLCgP5DKpNM_xrCJvX66Nc",
-        libraries: ["places"],
-      });
-    
-      const [selected, setSelected] = useState(null);
-    
-    if (!isLoaded) return <div>Loading...</div>;
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: "AIzaSyC_2ABFZTAwaDLCgP5DKpNM_xrCJvX66Nc",
+    libraries: ["places"],
+  });
+
+  const [selected, setSelected] = useState(null);
+
+  useEffect(() => {
+    return () => {};
+  }, []);
+
+  if (!isLoaded) return <div>Loading...</div>;
   return (
     <>
       {/* <BrandNavbar /> */}
 
       {/* Search Section */}
       <div>
-    
-
-        <Container className=''>
+        <Container className="">
           <Form.Label htmlFor="locSearch">Search Your Location</Form.Label>
           {/*<Form.Control
             type="text"
@@ -44,49 +47,48 @@ const LandingPage = () => {
           <Form.Text id="LocationSearch" muted>
             Search for you desired location!
           </Form.Text>
-
         </Container>
       </div>
     </>
-  )
-}
+  );
+};
 const PlacesAutocomplete = ({ setSelected }) => {
-    const {
-      ready,
-      value,
-      setValue,
-      suggestions: { status, data },
-      clearSuggestions,
-    } = usePlacesAutocomplete();
-  
-    const handleSelect = async (address) => {
-      setValue(address, false);
-      clearSuggestions();
-  
-      const results = await getGeocode({ address });
-      const { lat, lng } = await getLatLng(results[0]);
-      setSelected({ lat, lng });
-    };
+  const {
+    ready,
+    value,
+    setValue,
+    suggestions: { status, data },
+    clearSuggestions,
+  } = usePlacesAutocomplete();
 
-    return (
-      <Combobox  onSelect={handleSelect}>
-        <ComboboxInput
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-          disabled={!ready}
-          className="combobox-input form-control rounded-5 p-2"
-          placeholder="Search an address"
-        />
-        <ComboboxPopover>
-          <ComboboxList>
-            {status === "OK" &&
-              data.map(({ place_id, description }) => (
-                <ComboboxOption key={place_id} value={description} />
-              ))}
-          </ComboboxList>
-        </ComboboxPopover>
-      </Combobox>
-    );
+  const handleSelect = async (address) => {
+    setValue(address, false);
+    clearSuggestions();
+
+    const results = await getGeocode({ address });
+    const { lat, lng } = await getLatLng(results[0]);
+    setSelected({ lat, lng });
   };
 
-export default LandingPage
+  return (
+    <Combobox onSelect={handleSelect}>
+      <ComboboxInput
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        disabled={!ready}
+        className="combobox-input form-control rounded-5 p-2"
+        placeholder="Search an address"
+      />
+      <ComboboxPopover>
+        <ComboboxList>
+          {status === "OK" &&
+            data.map(({ place_id, description }) => (
+              <ComboboxOption key={place_id} value={description} />
+            ))}
+        </ComboboxList>
+      </ComboboxPopover>
+    </Combobox>
+  );
+};
+
+export default LandingPage;
