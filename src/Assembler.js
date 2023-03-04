796410a9174e29fd58041826a74d7178ee1573af
Alankrit Arya <alankritarya15@gmail.com>
2023-03-04 17:18:36 +0530

    route

--- src/Assembler.js
+++ src/Assembler.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import BrandNavbar from "./components/common/BrandNavbar";
 import LandingPage from "./components/LandingPage/LandingPage";
+import Dashboard from "./components/Dashboard/Dashboard"
 import Home from "./components/Home/Home";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { Col, Row } from "react-bootstrap";
@@ -15,6 +16,7 @@ const Assembler = () => {
       <Router>
         <Routes>
           <Route path="/home" exact element={<Home />} />
+          <Route path="/dash" exact element={<Dashboard />} />
           <Route path="/" exact element={<LandingPage />} />
         </Routes>
       </Router>
