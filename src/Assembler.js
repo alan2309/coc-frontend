90ffd6cb42aef835736dfb9350c259292262e073
chaitanya_1911 <chaitanya.kumbhar@think360.ai>
2023-03-04 18:33:48 +0530

    lat long added

--- src/Assembler.js
+++ src/Assembler.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import BrandNavbar from "./components/common/BrandNavbar";
 import LandingPage from "./components/LandingPage/LandingPage";
-import Dashboard from "./components/Dashboard/Dashboard"
+import Dashboard from "./components/Dashboard/Dashboard";
 import Home from "./components/Home/Home";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { Col, Row } from "react-bootstrap";
@@ -16,7 +16,7 @@ const Assembler = () => {
     <>
       <Router>
         <Routes>
-          <Route path="/home" exact element={<Home />} />
+          <Route path="/home/:lat/:lng" exact element={<Home />} />
           <Route path="/dash" exact element={<Dashboard />} />
           <Route path="/" exact element={<LandingPage />} />
           <Route path="/mapdata" exact element={<MapData />} />
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,27 +1,29 @@
-import React from 'react';
-import BrandNavbar from '../common/BrandNavbar';
-import { Container } from 'react-bootstrap';
-import TopNavSections from './TopNavSections';
-import Feed from './Feed/Feed';
+import React from "react";
+import BrandNavbar from "../common/BrandNavbar";
+import { Container } from "react-bootstrap";
+import TopNavSections from "./TopNavSections";
+import Feed from "./Feed/Feed";
+import { useParams } from "react-router";
 
 const Home = () => {
-
+  const { lat, lng } = useParams();
+  console.log(lat, lng);
   const HomeDash = () => {
     return (
       <>
         <BrandNavbar />
-        <Container className='mt-5 p-0'>
+        <Container className="mt-5 p-0">
           <TopNavSections />
           {/* <Feed /> */}
         </Container>
       </>
-    )
-  }
+    );
+  };
   return (
     <>
       <HomeDash />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
