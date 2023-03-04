c5532909b30fabde8a3f04f5bc9b150a641ffb33
chaitanya_1911 <chaitanya.kumbhar@think360.ai>
2023-03-04 15:42:20 +0530

    added asembler

--- src/Assembler.js
+++ src/Assembler.js
@@ -1,7 +1,10 @@
-import React, { useContext } from 'react'
-import BrandNavbar from './components/common/BrandNavbar'
-import LandingPage from './components/LandingPage/LandingPage'
-import Home from './components/Home/Home'
+import React, { useContext } from "react";
+import BrandNavbar from "./components/common/BrandNavbar";
+import LandingPage from "./components/LandingPage/LandingPage";
+import Home from "./components/Home/Home";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Col, Row } from "react-bootstrap";
+
 // import { AppBinderContext } from './AppBinderContext';
 
 const Assembler = () => {
@@ -9,9 +12,34 @@ const Assembler = () => {
 
   return (
     <>
-      <Home />
+      <Router>
+        <Routes>
+          <Route
+            path="/home"
+            exact
+            element={
+              <Row style={{ margin: "20px 0 0 0" }}>
+                <Col style={{ padding: "10px 90px" }}>
+                  <Home />
+                </Col>
+              </Row>
+            }
+          />
+          <Route
+            path="/"
+            exact
+            element={
+              <Row style={{ margin: "20px 0 0 0" }}>
+                <Col style={{ padding: "10px 90px" }}>
+                  <LandingPage />
+                </Col>
+              </Row>
+            }
+          />
+        </Routes>
+      </Router>
     </>
-  )
-}
+  );
+};
 
-export default Assembler
+export default Assembler;
